e8531eae1d16f436a740dda568a4483135252c82
nicolas <nicolas.anastacio.pereira@escola.pr.gov.br>
2025-08-06 21:12:47 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -2,6 +2,6 @@ let n1;
 let res = document.getElementById(`resultado`);
 function multi(){ 
     n1 = Number(window.prompt("Digite distância em km"));
-    res = n1 * 2.2;
-    resultado.innerHTML = `<p> Você irá pagar ${res.toFixed(1)}. </p>`
+    res = n1 * 2.6;
+    resultado.innerHTML = `<p> Você irá pagar R$${res.toFixed(1)}. </p>`
 }
